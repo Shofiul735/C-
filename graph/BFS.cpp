becf27c15d174ff6b61314d0b4a38865d5182e40
Md. Shofiul Islam <ytshofiul@gmail.com>
2022-05-25 16:10:02 +0600

    BFS implemented

---
+++ graph/BFS.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Graph{
+    unordered_map<int,vector<int>> graph;
+    vector<int> visited;
+    queue<int> q;
+    public:
+        void addEdge(int v,int e,bool undir=true){
+            if(!count(graph[v].begin(),graph[v].end(),e)){
+                graph[v].push_back(e);
+            }
+            if(!count(graph[e].begin(),graph[e].end(),v) and undir){
+                graph[e].push_back(v);
+            }
+        }
+
+        void printAll(){
+            for(auto m:graph){
+                cout<<m.first<<"--> ";
+                for(auto i:m.second){
+                    cout<<i<<" ";
+                }
+                cout<<endl;
+            }
+        }
+
+        vector<int> BFS(int start){
+            int vertex;
+            this->visited.push_back(start);
+            for(auto m:this->graph[start]){
+                if((find(this->visited.begin(),this->visited.end(),m) == this->visited.end())){
+                        q.push(m);
+                    }
+            }
+            while(!q.empty()){
+                vertex = q.front();
+                q.pop();
+                if(find(this->visited.begin(),this->visited.end(),vertex) == this->visited.end()){
+                    visited.push_back(vertex);
+                }
+                for(auto x:this->graph[vertex]){
+                    if((find(this->visited.begin(),this->visited.end(),x) == this->visited.end())){
+                        q.push(x);
+                    }
+                }
+            }
+            return this->visited;
+        }
+
+};
+
+int main(){
+    Graph g;
+    g.addEdge(0,1);
+    g.addEdge(0,4);
+    g.addEdge(1,0);
+    g.addEdge(1,2);
+    g.addEdge(2,1);
+    g.addEdge(2,3);
+    g.addEdge(3,2);
+    g.addEdge(3,4);
+    g.addEdge(4,0);
+    g.addEdge(4,3);
+    g.addEdge(4,5);
+    g.addEdge(5,3);
+    g.addEdge(5,4);
+    g.addEdge(5,6);
+    g.printAll();
+    vector<int> bfs = g.BFS(1);
+    cout<<"BFS: ";
+    for(auto i:bfs){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+
+    return 0;
+}
